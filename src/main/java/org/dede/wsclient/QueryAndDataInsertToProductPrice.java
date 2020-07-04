4dced0a06a6bc0d1512584f8d6be81e2afaced45
User <tutnatha>
2020-07-04 22:37:11 +0800

    trulalala

--- src/main/java/org/dede/wsclient/QueryAndDataInsertToProductPrice.java
+++ src/main/java/org/dede/wsclient/QueryAndDataInsertToProductPrice.java
@@ -254,4 +254,9 @@ public class QueryAndDataInsertToProductPrice extends AbstractTestWS {
 	          	}
 		}
 	}
+		
+	public static void main(String[] args) {
+		new QueryAndDataInsertToProductPrice();
+	}
+	
 }
